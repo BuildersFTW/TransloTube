515182bcfcbe440aaa5361305b77c3ea470b8a81
Rudra Lakhani <65751992+rudra-first@users.noreply.github.com>
2024-08-07 18:23:52 +0400

    changes

--- src/home/models.py
+++ src/home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class TaskStatus(models.Model):
     task_id = models.CharField(max_length=255, unique=True)
     status = models.TextField()
-    context = models.JSONField(blank=True)
+    context = models.JSONField(null=True)
 
     def __str__(self):
         return self.task_id
